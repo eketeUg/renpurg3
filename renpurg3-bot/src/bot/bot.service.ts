8ede4441b988a9fae71348229da2baee6dca12d5
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-31 21:07:00 +0000

    feat: rentpurg3 bot

--- renpurg3-bot/src/bot/bot.service.ts
+++ renpurg3-bot/src/bot/bot.service.ts
@@ -16,7 +16,7 @@ const token = process.env.TELEGRAM_TOKEN;
 
 @Injectable()
 export class BotService {
-  private readonly gaslessBot: TelegramBot;
+  private readonly renPurge3Bot: TelegramBot;
   private logger = new Logger(BotService.name);
 
   constructor(
@@ -24,15 +24,15 @@ export class BotService {
     private readonly reclaimService: ReclaimService,
     @InjectModel(User.name) private readonly UserModel: Model<User>,
   ) {
-    this.gaslessBot = new TelegramBot(token, { polling: true });
-    this.gaslessBot.on('message', this.handleRecievedMessages);
-    this.gaslessBot.on('callback_query', this.handleButtonCommands);
+    this.renPurge3Bot = new TelegramBot(token, { polling: true });
+    this.renPurge3Bot.on('message', this.handleRecievedMessages);
+    this.renPurge3Bot.on('callback_query', this.handleButtonCommands);
   }
 
   handleRecievedMessages = async (msg: any) => {
     this.logger.debug(msg);
     try {
-      await this.gaslessBot.sendChatAction(msg.chat.id, 'typing');
+      await this.renPurge3Bot.sendChatAction(msg.chat.id, 'typing');
 
       const walletRegex = /^\s*([1-9A-HJ-NP-Za-km-z]{32,44})\s*$/;
 
@@ -47,14 +47,6 @@ export class BotService {
         await this.scanProvider(msg.chat.id, walletAddress);
       }
 
-      if (
-        msg.text !== '/start' &&
-        msg.text !== '/menu' &&
-        msg.text !== '/cancel' &&
-        msg.text !== '/balance'
-      ) {
-        // return this.handleUserTextInputs(msg, session!);
-      }
       const command = msg.text!;
       console.log('Command :', command);
 
@@ -74,7 +66,7 @@ export class BotService {
         welcome = await welcomeMessageMarkup(username);
         if (welcome) {
           const replyMarkup = { inline_keyboard: welcome.keyboard };
-          await this.gaslessBot.sendMessage(msg.chat.id, welcome.message, {
+          await this.renPurge3Bot.sendMessage(msg.chat.id, welcome.message, {
             reply_markup: replyMarkup,
             parse_mode: 'HTML',
           });
@@ -87,14 +79,18 @@ export class BotService {
         const allFeatures = await allFeaturesMarkup();
         if (allFeatures) {
           const replyMarkup = { inline_keyboard: allFeatures.keyboard };
-          await this.gaslessBot.sendMessage(msg.chat.id, allFeatures.message, {
-            parse_mode: 'HTML',
-            reply_markup: replyMarkup,
-          });
+          await this.renPurge3Bot.sendMessage(
+            msg.chat.id,
+            allFeatures.message,
+            {
+              parse_mode: 'HTML',
+              reply_markup: replyMarkup,
+            },
+          );
         }
       }
       if (command === '/cancel') {
-        return await this.gaslessBot.sendMessage(
+        return await this.renPurge3Bot.sendMessage(
           msg.chat.id,
           ' ✅All  active sessions closed successfully',
         );
@@ -134,12 +130,12 @@ export class BotService {
       const user = await this.UserModel.findOne({ chatId: chatId });
       switch (command) {
         case '/menu':
-          await this.gaslessBot.sendChatAction(chatId, 'typing');
+          await this.renPurge3Bot.sendChatAction(chatId, 'typing');
           await this.sendAllFeature(user);
           return;
 
         case '/scanDefault':
-          await this.gaslessBot.sendChatAction(chatId, 'typing');
+          await this.renPurge3Bot.sendChatAction(chatId, 'typing');
           await this.scanProvider(chatId);
           return;
 
@@ -148,21 +144,27 @@ export class BotService {
 
         //   close opened markup and delete session
         case '/closeDelete':
-          await this.gaslessBot.sendChatAction(query.message.chat.id, 'typing');
-          return await this.gaslessBot.deleteMessage(
+          await this.renPurge3Bot.sendChatAction(
+            query.message.chat.id,
+            'typing',
+          );
+          return await this.renPurge3Bot.deleteMessage(
             query.message.chat.id,
             query.message.message_id,
           );
 
         case '/close':
-          await this.gaslessBot.sendChatAction(query.message.chat.id, 'typing');
-          return await this.gaslessBot.deleteMessage(
+          await this.renPurge3Bot.sendChatAction(
+            query.message.chat.id,
+            'typing',
+          );
+          return await this.renPurge3Bot.deleteMessage(
             query.message.chat.id,
             query.message.message_id,
           );
 
         default:
-          return await this.gaslessBot.sendMessage(
+          return await this.renPurge3Bot.sendMessage(
             query.message.chat.id,
             `Processing command failed, please try again`,
           );
@@ -174,13 +176,13 @@ export class BotService {
 
   sendAllFeature = async (user: UserDocument) => {
     try {
-      await this.gaslessBot.sendChatAction(user.chatId, 'typing');
+      await this.renPurge3Bot.sendChatAction(user.chatId, 'typing');
       const allFeatures = await allFeaturesMarkup();
       if (allFeatures) {
         const replyMarkup = {
           inline_keyboard: allFeatures.keyboard,
         };
-        await this.gaslessBot.sendMessage(user.chatId, allFeatures.message, {
+        await this.renPurge3Bot.sendMessage(user.chatId, allFeatures.message, {
           parse_mode: 'HTML',
           reply_markup: replyMarkup,
         });
@@ -192,8 +194,8 @@ export class BotService {
 
   promptWalletInput = async (chatId: TelegramBot.ChatId) => {
     try {
-      await this.gaslessBot.sendChatAction(chatId, 'typing');
-      await this.gaslessBot.sendMessage(
+      await this.renPurge3Bot.sendChatAction(chatId, 'typing');
+      await this.renPurge3Bot.sendMessage(
         chatId,
         `enter the provider wallet you want to scan`,
         {
@@ -213,7 +215,7 @@ export class BotService {
     providerWallet?: string,
   ) => {
     try {
-      await this.gaslessBot.sendChatAction(chatId, 'typing');
+      await this.renPurge3Bot.sendChatAction(chatId, 'typing');
       const result = await this.reclaimService.scanProviderWallet(
         providerWallet ? providerWallet : process.env.KORA_PROVIDER_PUBKEY,
       );
@@ -223,14 +225,14 @@ export class BotService {
         const replyMarkup = {
           inline_keyboard: markup.keyboard,
         };
-        await this.gaslessBot.sendMessage(chatId, markup.message, {
+        await this.renPurge3Bot.sendMessage(chatId, markup.message, {
           parse_mode: 'HTML',
           reply_markup: replyMarkup,
         });
 
         return;
       } else {
-        await this.gaslessBot.sendMessage(
+        await this.renPurge3Bot.sendMessage(
           chatId,
           `No reclaimable ATAs found for the provided wallet.`,
           {
--- renpurg3-bot/src/main.ts
+++ renpurg3-bot/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3521);
+  await app.listen(3522);
 }
 bootstrap();
